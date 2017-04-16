18fc3d15fc641efa4ef31259e1e8267aa1efdff8
Gautham Kumar <heygautham@gmail.com>
2017-04-16 22:38:42 +0530

    Initial commit

---
+++ OMDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OMDB
+//
+//  Created by Gautham Kumar on 16/04/17.
+//  Copyright © 2017 Gautham Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
