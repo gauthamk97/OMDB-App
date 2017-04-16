554b5b89fab9445bc7c5b37dc1732491016f5b37
Gautham Kumar <heygautham@gmail.com>
2017-04-17 00:05:13 +0530

    Basic API Implementation

--- OMDB/RecentsViewController.swift
+++ OMDB/RecentsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RecentsViewController: UIViewController {
+class RecentsViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +21,56 @@ class RecentsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let movieName: String = searchBar.text!
+        let updatedMovieName = movieName.replacingOccurrences(of: " ", with: "+")
+        let urlToHit = URL(string: "http://www.omdbapi.com/?t=\(updatedMovieName)&plot=full") //Route to hit
+        var request = URLRequest(url: urlToHit!)
+        request.httpMethod = "POST"
+    
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+            
+            let httpStatus = response as? HTTPURLResponse
+            
+            if (error != nil) {
+                if (httpStatus?.statusCode == nil) {
+                    print("NO INTERNET")
+                    //                    NotificationCenter.default.post(name: NSNotification.Name(rawValue: "nointernetforclusters"), object: nil)
+                }
+                else {
+                    print("Error occured : \(error)")
+                }
+                return;
+            }
+                
+            else if httpStatus?.statusCode != 200 {
+                print("Error : HTTPStatusCode is \(httpStatus?.statusCode)")
+                //            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "servererror"), object: nil)
+                return
+            }
+                
+            else {
+                let responseString = String(data: data!, encoding: .utf8)
+                let jsonData = responseString?.data(using: .utf8)
+                if let json = try? JSONSerialization.jsonObject(with: jsonData!) as! [String: Any] {
+                    
+                    if json["response"] as! String == "False" {
+                        //Alert saying movie doesn't exist
+                    }
+                    
+                    else {
+                        print(json)
+                    }
+                    
+                }
+                
+            }})
+        
+        task.resume()
+        
     }
     
 
